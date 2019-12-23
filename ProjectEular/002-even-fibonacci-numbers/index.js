66d59ec74862f2a46a2925b9e664b500c4a27138
Micheal Koblinski <stevenkoblinskip@gmail.com>
2019-12-23 16:19:52 -0500

    finished the fibonacci sequence

--- ProjectEular/002-even-fibonacci-numbers/index.js
+++ ProjectEular/002-even-fibonacci-numbers/index.js
@@ -1,5 +1,16 @@
+let y = 0
+let z = 1
+let x = 1
+console.log(y)
+console.log(z)
+console.log(x)
 const fibonacci = () => {
-  const x = 1
-  const y = x + z
-  const z = x + y
+  for(let i = 0; i < 4000000; i = z) {
+    y = x + z
+    z = z + y
+    x = y
+    console.log(y)
+    console.log(z)
+  }
 }
+fibonacci()
