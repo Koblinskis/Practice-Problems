69432f12077eee2ac7cf4f51752ed2234992b662
Micheal Koblinski <stevenkoblinskip@gmail.com>
2019-12-23 19:43:28 -0500

    solved problem 4

--- ProjectEular/004-largest-palindrome-product/index.js
+++ ProjectEular/004-largest-palindrome-product/index.js
@@ -1,6 +1,24 @@
-for(let i = 999; i > 100; i--) {
-  for(let j = 999; j > 100; j--) {
-    const num = j*i;
-    console.log(num)
+const palinArr = []
+
+/**
+ * cycles through multipling three-digit number and stores all the palindrome number in an array
+ */
+const palindromeNumber = () => {
+  for(let i = 999; i > 100; i--) {
+    for(let j = 999; j > 100; j--) {
+      const num = j*i;
+      if(num.toString() === num.toString().split('').reverse().join('')) {
+        palinArr.push(num)
+      }
+    }
   }
 }
+palindromeNumber()
+
+/**
+ * looks through an array and finds the largest number
+ */
+const palinLargestNum = (arr) => {
+  console.log(Math.max(...arr))
+}
+palinLargestNum(palinArr)
